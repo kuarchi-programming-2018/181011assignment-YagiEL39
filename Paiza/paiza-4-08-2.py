87d71d653db034fe535450479844fc78a00e2df5
YagiEL39 <43873702+YagiEL39@users.noreply.github.com>
2018-10-07 19:20:05 +0900

    181011課題終了しました。(課題提出が一番難しい...)

--- Paiza/paiza-4-08-2.py
+++ Paiza/paiza-4-08-2.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+﻿# coding: utf-8
 
 '''
 演習課題「おみくじを作ろう」
@@ -26,6 +26,10 @@ line = input().rstrip()
 # 今回は自力で全部書いてみよう！
 
 # カンマで分割して、リストに代入
+list=line.split(",")
 # リストの要素数を変数に代入
+num=len(list)
 # リストの中身を出力
+print(list)
 # ランダムに選んだリストの要素を出力
+print(list[random.randrange(num)])
